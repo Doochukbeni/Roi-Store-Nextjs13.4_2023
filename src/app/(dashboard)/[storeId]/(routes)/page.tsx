b5748ec0f804ae43efb86fa820dc6abf04a472e0
doochukbeni <doochukbeni@gmail.com>
2023-08-28 23:27:55 +0400

    add stripe and webhook

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,17 @@
+import Heading from "@/components/Heading";
+import { Separator } from "@/components/ui/Separator";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
